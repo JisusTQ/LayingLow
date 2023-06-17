30bcff03afd489a36616a0f14b4f8de6caac343d
matnaranjo <matnaranjo2020@gmail.com>
2023-06-17 13:55:33 -0500

    Scientist logic when unaware of alien, Done

--- Assets/TestScripts Mateo/ScientistS/ScientistMovement.cs
+++ Assets/TestScripts Mateo/ScientistS/ScientistMovement.cs
@@ -7,32 +7,50 @@ public class ScientistMovement : MonoBehaviour
 
     #region movement variables
     int direction; // direction will change between 1 and -1
-    public float speed; // how fast the object will move in the screen
+    [Header("Movement Variables")]
+    [Tooltip("How fast the object will walk")]
+    public float walkSpeed; // how fast the object will walk
+    [Tooltip("How fast the object will run")]
+    public float runSpeed; // how fast the objct will run
     #endregion
 
     #region bools
     bool isEdge; // the character is in the edge of the screen
-    // bool 
+    bool isLookingForJob = true; //the character is choosing where to work 
+    bool isWorking; // The scientist reached the machine
     #endregion
+
+    #region ints
+    int chooseActivitie = 0;// index of the object where the character will "work" on
+    #endregion
+   #region Screen Objects
     
+    GameObject[] funActivities; //stores the positions of every activity the scientist can perform if they are unaware of danger and decide to work
+    Transform redButton; // if the scientist discovers the alien, here it will call security.
+
+    #endregion
 
     # region status
     ScientistStatus status;
     # endregion
+    
+    
     void Start()
-    {
+    {   
+        redButton = GameObject.FindGameObjectWithTag("redbutton").GetComponent<Transform>();
+        funActivities = GameObject.FindGameObjectsWithTag("funnies");
         status = GetComponent<ScientistStatus>();
-        RandomStart();
+        StartCoroutine(Choosetask());
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Walk();
+        TaskExe();
     }
 
     ///<summary>
-    ///Description: While scientist are unaware of the alien presence they will just walk in the room.<br/>
+    ///Description: While scientist are unaware of the alien presence (status.Suspicion = none) they will just walk in the room. If they reach the edge of the room, they will change their direction of movement<br/>
     ///Input: None<br/>
     ///Return: None
     ///</summary>
@@ -40,7 +58,7 @@ public class ScientistMovement : MonoBehaviour
 
         Vector3 currentPos = transform.position;
 
-        Vector3 step= new Vector3(speed,0,0)*Time.fixedDeltaTime;
+        Vector3 step= new Vector3(walkSpeed,0,0)*Time.fixedDeltaTime;
 
         if(currentPos.x<10 && currentPos.x>-10){
             transform.position+=step*direction;
@@ -50,18 +68,83 @@ public class ScientistMovement : MonoBehaviour
         direction *=-1;
 
         transform.position +=step*direction;
-
-
-
-
     }
 
     ///<summary>
-    ///Description: Called at the Start(), here, all the needed data for movement and behavior will have a random set up<br/>
+    ///Description: If scientists discover the alien, they will run toward the red button to call security running left or right, based on the X distance between the scientist and the button.<br/>
     ///Input: None<br/>
     ///Return: None
     ///</summary>
-    private void RandomStart(){
-        direction = Random.Range(-1, 2);
+    private void Run(){
+        int redButtonDir;
+        float distanceToRedButton = redButton.position.x - transform.position.x;
+
+        redButtonDir = (distanceToRedButton<0)?-1:1;
+
+        Vector3 step = new Vector3(runSpeed, 0, 0)* Time.fixedDeltaTime * redButtonDir;
+        transform.position += step;
     }
+
+    ///<summary>
+    /// Description: If Work, gets selected as the task status, the scientist will choose a random object to work on, they will walk to it based on the position and do their bussiness<br/>
+    ///Input: None<br/>
+    ///Return: None
+    ///</summary>
+    private void Work(){
+        
+        if(isLookingForJob){
+            chooseActivitie = Random.Range(0, funActivities.Length);
+            isLookingForJob=false;
+        }
+
+        if (!isWorking)
+        {
+            int workDir;
+            float distanceToWork = funActivities[chooseActivitie].transform.position.x - transform.position.x;
+
+            workDir = (distanceToWork<0)? - 1:1;
+            
+            Vector3 step = new Vector3(walkSpeed, 0, 0)* Time.fixedDeltaTime * workDir;
+            transform.position += step;
+        }
+        if (transform.position.x <(funActivities[chooseActivitie].transform.position.x+0.1) && transform.position.x >(funActivities[chooseActivitie].transform.position.x-0.1)){
+            isWorking=true;
+        }
+    }
+
+    IEnumerator Choosetask(){
+        while(true){
+            Debug.Log(status.GetTask());
+            direction = (status.GetDir()==ScientistStatus.Dir.right)?1:-1;
+            yield return new WaitForSeconds(15);
+            int randTask = Random.Range(0,3);
+            switch (randTask)
+            {
+                case 0:
+                    status.SetTask(ScientistStatus.Task.walk);
+                break;
+                
+                case 1:
+                    status.SetTask(ScientistStatus.Task.work);
+                break;
+                
+                case 2:
+                    status.SetTask(ScientistStatus.Task.idle);
+                break;
+            }
+            isLookingForJob = true;
+            isWorking = false;
+            
+        }
+    }
+
+    private void TaskExe(){
+        if (status.GetTask()==ScientistStatus.Task.walk){
+            Walk();
+        }
+        else if (status.GetTask()==ScientistStatus.Task.work){
+            Work();
+        }
+    }
+
 }
--- Assets/TestScripts Mateo/ScientistS/ScientistStatus.cs
+++ Assets/TestScripts Mateo/ScientistS/ScientistStatus.cs
@@ -6,8 +6,7 @@ public class ScientistStatus : MonoBehaviour
 {
     public enum Dir{
         left,
-        right,
-        none
+        right
     }
 
     public enum Suspicion{
@@ -16,9 +15,21 @@ public class ScientistStatus : MonoBehaviour
         discovered
     }
 
-    Dir movementDir;
-    Suspicion susStatus;
+    public enum Task{
+        walk,
+        work,
+        idle
+    }
 
+    public Dir movementDir;
+    Suspicion susStatus;
+    Task taskStatus;
+
+    public void Start(){
+        RandomStart();
+    }
+
+    #region Direction
     ///<summary>
     ///Description: Public function to <b>GET</b> the direction status<br/>
     ///Input: None<br/>
@@ -30,12 +41,96 @@ public class ScientistStatus : MonoBehaviour
     }
 
     ///<summary>
-    ///Description: Public function to <b>SET</b> the direction status<br/>
-    ///Input: 
-    ///Return: MovementDir status
+    ///Description: Public function to <b>SET</b> the direction status to <paramref name="dir"/> provided as parameter <br/>
+    ///Input: <paramref name="dir"/><br/>
+    ///Return: None
     ///</summary>
     public void SetDir(Dir dir)
     {
         movementDir = dir;
     }
+    #endregion
+
+    #region Suspicion
+    ///<summary>
+    ///Description: Public function to <b>GET</b> the Suspicion status<br/>
+    ///Input: None<br/>
+    ///Return: susStatus status
+    ///</summary>
+    public Suspicion GetSus()
+    {
+        return susStatus;
+    }
+
+    ///<summary>
+    ///Description: Public function to <b>SET</b> the Suspicion status to <paramref name="sus"/> provided as parameter <br/>
+    ///Input: <paramref name="sus"/><br/>
+    ///Return: None
+    ///</summary>
+    public void SetSus(Suspicion sus)
+    {
+        susStatus = sus;
+    }
+    #endregion
+
+    #region Task
+    ///<summary>
+    ///Description: Public function to <b>GET</b> the task status<br/>
+    ///Input: None<br/>
+    ///Return: taskStatus status
+    ///</summary>
+    public Task GetTask()
+    {
+        return taskStatus;
+    }
+
+    ///<summary>
+    ///Description: Public function to <b>SET</b> the task status to <paramref name="task"/> provided as parameter<br/>
+    ///Input: <paramref name="task"/><br/>
+    ///Return: None
+    ///</summary>
+    public void SetTask(Task task)
+    {
+        taskStatus = task;
+    }
+    #endregion
+
+    ///<summary>
+    ///Description: Called at the Start(), here, all the needed data for movement and behavior will have a random set up.<br/>
+    ///- Direction and task will be randomly choosen<br/>
+    ///Input: None<br/>
+    ///Return: None
+    ///</summary>
+    private void RandomStart(){
+        int randDir = Random.Range(0,2);
+        int randTask = Random.Range(0,3);
+
+        switch (randDir)
+        {
+            case 0:
+                SetDir(Dir.left);
+            break;
+
+            case 1:
+                SetDir(Dir.right);
+            break;
+        }
+
+        switch (randTask)
+        {
+            case 0:
+                SetTask(Task.walk);
+            break;
+            
+            case 1:
+                SetTask(Task.work);
+            break;
+            
+            case 2:
+                SetTask(Task.idle);
+            break;
+        }
+        
+    }
+
 }
